0282151e3d1938aa3270728e4c6fcd742cf28cfb
Takuya Tsuchida <tsucchi@cpan.org>
2012-10-16 14:45:05 +0900

    initial commit

---
+++ lib/Test/DataLoader.pm
@@ -0,0 +1,32 @@
+package Test::DataLoader;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Test::DataLoader -
+
+=head1 SYNOPSIS
+
+  use Test::DataLoader;
+
+=head1 DESCRIPTION
+
+Test::DataLoader is
+
+=head1 AUTHOR
+
+Takuya Tsuchida E<lt>tsucchi {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
